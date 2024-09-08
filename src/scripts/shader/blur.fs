3b20dc41e3200413730c979d60a0fd4059deafb9
nemutas <inohiro.focru@gmail.com>
2024-09-08 10:15:43 +0900

    .

--- src/scripts/shader/blur.fs
+++ src/scripts/shader/blur.fs
@@ -12,21 +12,11 @@ const float PI = acos(-1.0);
 const float DIV2 = 2.0 * pow(1.0, 2.0); // 2 * σ^2
 const float SAMPLING = 3.0;
 
-vec4 gaussianBlur(sampler2D src, vec2 uv, vec2 px, vec2 dir) {
-  vec4 sum = vec4(0.0);
-
-  for (float i = -SAMPLING; i <= SAMPLING; i++) {
-    float g = (1.0 / sqrt(PI * DIV2)) * exp(-(i * i) / DIV2);
-    sum += texture(src, uv + i * px * dir) * g;
-  }
-
-  return sum;
-}
-
 void main() {
   vec2 uv = vUv;
   vec2 px = 1.0 / resolution;
 
+  // gaussianBlur
   vec4 sum;
   for (float i = -SAMPLING; i <= SAMPLING; i++) {
     float g = (1.0 / sqrt(PI * DIV2)) * exp(-(i * i) / DIV2);
